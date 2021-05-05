96bf9a2ef35e0408fa46d101969de4dcda2e7683
OscarRamos98 <osenjaimera@ittepic.edu.mx>
2021-05-04 19:21:01 -0600

    Se agregó lo que faltaba del CRUD

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ mongoose.connect('mongodb://localhost:27017/eje05');
 var Book = mongoose.model('Book',schema,'books');
 
 var book = new Book({
-    title: 'Paco el chato',
-    author: 'Oscar Ramos',
-    body: 'Buen libro',
+    title: 'Harry Potter',
+    author: 'Enrique Jaime',
+    body: 'Ssupenso',
     comments: [{
         body:'Buen libro',
         date:'2021/05/03'
@@ -24,5 +24,37 @@ book.save(function(error) {
         process.exit(1);
     } 
     console.log("Guardado con exito!");
-    process.exit(0);
+    //process.exit(0);
  });
+
+Book.update({_id:'60900c4c54b3a63ac41f711d'}, {$set:{hidden: true}},
+function (error, docs) {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("<----Actualizacion---->");
+    console.log(docs);
+    //process.exit(0);
+});
+
+Book.findOneAndRemove({_id:'60900c4c54b3a63ac41f711d'}, function(error, docs) {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log("Se eliminó correctamente");
+    console.log(docs);
+    process.exit(0);
+});
+
+Book.find({author: "Oscar Ramos"}, 
+function (error, docs) {
+    if(error){
+        console.log(error);
+        process.exit(1);
+    }
+console.log("<----Consulta---->");
+console.log(docs);
+process.exit(0);
+});
